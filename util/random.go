9e1d5fe780d50a2834eee23c3a720c21714faa29
Parth <parth@shoppinpal.com>
2022-07-28 00:37:21 +0530

    pushed

---
+++ util/random.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+
+}
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+const alphabet = "abcdefghijkmnopqrstuvwxyz"
+
+func RandString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+func RandomOwner() string {
+	return RandString(6)
+}
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "INR"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
